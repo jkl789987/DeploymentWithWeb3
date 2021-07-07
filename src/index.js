4b1bb63cea7dab120f9c17c3874402c5dfcc6c70
jlance <jacklance1@gmail.com>
2021-07-06 18:00:15 -0600

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ 
+// eslint-disable-next-line
+import React, { Children } from 'react';
+// eslint-disable-next-line
+import ReactDom from 'react-dom';
+ 
+const books = [
+ {
+   id:1,
+   img: "https://images-na.ssl-images-amazon.com/images/I/81PlDZAxJDL._UX300__PJku-sticker-v7,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL210_SR210,210_.jpg",
+   title: "Blockchain",
+   author: "Jack"
+ },
+ {
+   id:2,
+   img: "https://m.media-amazon.com/images/I/71kxa1-0mfL._AC_AA180_.jpg",
+   title: "1984",
+   author: "Anthony Lewis"
+ },
+ {
+   id:3,
+   img: "https://m.media-amazon.com/images/I/71kxa1-0mfL._AC_AA180_.jpg",
+   title: "1984",
+   author: "George Orwell"
+ }
+];
+ 
+// eslint-disable-next-line
+function BookList() {
+ return (
+   <section className='booklist'>
+       {books.map((book,index) => {
+         return <Book key={book.id} book={book}></Book>; //it will be equal to a book
+       })}
+   </section>
+ );
+}
+const Book = (props) => {
+ console.log(props);
+ const { img, title, author} = props.book;
+ 
+ return (
+   <article className='book'>
+     <img src ={img} alt=''/>
+     <h1>{title}</h1>
+     <h4>{author}</h4>
+    
+   </article>
+ );
+};
+
+
